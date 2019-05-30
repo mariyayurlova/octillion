8c008ecea68178ebc203ddc6fb37110fae9ffb9c
Маша Юрлова <mariya.yurlova@gmail.com>
2019-05-30 13:02:30 +0300

    init

---
+++ assets/templates/home.js
@@ -0,0 +1,12 @@
+import './../common/_common.scss';
+
+/**
+ * Add Mastak Library
+ */
+import './../mastak-lib/loader/loader';
+import './../components/header/header';
+import './../components/footer/footer';
+
+
+
+
