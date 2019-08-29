0d163416bef54f521eda35fb450c3f8b52a0d13f
zankoav <zankoav@mail.ru>
2019-08-29 17:22:20 +0300

    change_header

--- header.php
+++ header.php
@@ -5,6 +5,16 @@
     <meta name="viewport" content="initial-scale=1.0, width=device-width">
     <meta charset="<?php bloginfo('charset'); ?>">
     <?php wp_head(); ?>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-115763355-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-115763355-1');
+</script>
+</head>
 <body class="body_overflow_hidden app">
 <div class="loader">
     <div class="loader__spinner loader__spinner_egg"></div>
